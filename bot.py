1c64ec3391b483c4d759849b910a77ceadeb5547
Tony Li <tonyli@cs.stonybrook.edu>
2018-07-18 17:19:47 -0400

    added delay to loop

--- bot.py
+++ bot.py
@@ -44,8 +44,7 @@ for m in main_group.members:
                     'end_time': 0
                         
     })
-
-
+    
 
 
 
@@ -101,30 +100,33 @@ while True:
                     
 
     now = datetime.datetime.now()
-    
+
+    # Punch in reminder at 8:28 AM weekdays
     if now.isoweekday() < 5 and now.hour == 8 + offset and now.minute == 28 and not start_punch:
         jack_jack.post(text = 'Punch in')
         start_punch = True
 
-    
+    # Punch out reminder at 8:28 AM weekdays
     if now.isoweekday() < 5 and now.hour == 17 + offset and now.minute == 28 and not end_punch:
         jack_jack.post(text = 'Punch out')
         end_punch = True
 
-
+    # Theme day reminder at 12:45 PM on Wednesdays
     if now.isoweekday() == WED and now.hour == 12 + offset and now.minute == 45 and not theme_day:
         jack_jack.post(text = 'Remember to do theme day')
         theme_day = True
 
+    # Punch in reminder at 5:58 PM Thursdays
     if now.isoweekday() == THU and now.hour == 17 + offset and now.minute == 58 and not diploma_start_punch:
         jack_jack.post(text = 'Punch in for diplomas')
         diploma_start_punch = True
 
+    # Punch out at 8:28 PM Thursdays
     if now.isoweekday() == FRI and now.hour == 0 and now.minute == 28 and not diploma_end_punch:
         jack_jack.post(text = 'Punch out for diplomas')
         diploma_end_punch = True
         
-    # Reset vars
+    # Reset vars at midnight
     if now.hour == 0 + offset and now.minute == 0:
         start_punch = False
         end_punch = False
@@ -132,3 +134,4 @@ while True:
         diploma_start_punch = False
         diploma_end_punch = False
         
+    time.sleep(0.25)
